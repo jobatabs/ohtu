b0b964cf4ad09cb93ff851ae64fe480efb6d14c3
jobatabs <2370315+jobatabs@users.noreply.github.com>
2024-10-31 15:00:51 +0200

    Viikko 2 tht 2

--- viikko2/project-reader/src/project.py
+++ viikko2/project-reader/src/project.py
@@ -1,7 +1,9 @@
 class Project:
-    def __init__(self, name, description, dependencies, dev_dependencies):
+    def __init__(self, name, description, license, authors, dependencies, dev_dependencies):
         self.name = name
         self.description = description
+        self.license = license
+        self.authors = authors
         self.dependencies = dependencies
         self.dev_dependencies = dev_dependencies
 
@@ -9,9 +11,17 @@ class Project:
         return ", ".join(dependencies) if len(dependencies) > 0 else "-"
 
     def __str__(self):
-        return (
-            f"Name: {self.name}"
-            f"\nDescription: {self.description or '-'}"
-            f"\nDependencies: {self._stringify_dependencies(self.dependencies)}"
-            f"\nDevelopment dependencies: {self._stringify_dependencies(self.dev_dependencies)}"
-        )
+        reply = f"Name: {self.name}" + f"\nDescription: {self.description or '-'}" + \
+            f"\nLicense: {self.license or '-'}" + \
+            f"\n"
+        
+        reply = reply + f"\nAuthors:"
+        for author in self.authors:
+            reply = reply + f"\n- {author}"
+        reply = reply + f"\n\nDependencies:"
+        for dep in self.dependencies:
+            reply = reply + f"\n- {dep}"
+        reply = reply + f"\n\nDevelopment dependencies:"
+        for dep in self.dev_dependencies:
+            reply = reply + f"\n- {dep}"
+        return reply
