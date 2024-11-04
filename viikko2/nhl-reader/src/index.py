2d8c7d172502ddd1a664002e443c7ebe43882c24
jobatabs <2370315+jobatabs@users.noreply.github.com>
2024-11-04 16:36:51 +0200

    Add NHL reader

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,37 @@
-from player import Player
+from player import PlayerReader, PlayerStats
+from rich import print
+from rich.prompt import Prompt
+from rich.console import Console
+from rich.table import Table
+import signal, sys
 
-def main():
-    pass
+def main(console):
+    console.print("[italic grey]NHL statistics by nationality")
+    season = Prompt.ask("Select season [bold purple][2022-23/2023-24/2024-25][/bold purple]")
+    while True:
+        nationality = Prompt.ask("Select nationality [bold purple][FIN/LAT/AUT/CZE][/bold purple]")
+        url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+        reader = PlayerReader(url)
+        stats = PlayerStats(reader)
+        players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Top scorers of {nationality} season {season}")
+        table.add_column("name", justify="left", style="cyan")
+        table.add_column("team", style="purple")
+        table.add_column("goals", justify="right", style="green")
+        table.add_column("assists", justify="right", style="green")
+        table.add_column("points", justify="right", style="green")
+
+        for p in players:
+            table.add_row(p.name, p.team, str(p.goals), str(p.assists), str(p.goals+p.assists))
+        
+        console.print(table)
+
+def signal_hander(sig, frame):
+    console.print("\nExiting")
+    sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    console = Console()
+    signal.signal(signal.SIGINT, signal_hander)
+    main(console)
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,37 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team = dict['team']
+        self.nationality = dict['nationality']
+        self.assists = dict['assists']
+        self.goals = dict['goals']
+        self.games = dict['games']
+        self.id = dict['id']
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20}\t{self.team}\t{self.goals:2} + {self.assists:2} = {self.goals + self.assists}"
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.json = requests.get(url).json()
+        self.players = []
+        for player_dict in self.json:
+            player = Player(player_dict)
+            self.players.append(player)
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.players
+    
+    def top_scorers_by_nationality(self, nationality: str):
+        response = []
+
+        for player in self.players:
+            if player.nationality == nationality:
+                response.append(player)
+        
+        response.sort(key=lambda player: player.goals + player.assists, reverse=True)
+        return response
