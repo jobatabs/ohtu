22bec28374d68baa7f49bc8907201fb4093ca71c
jobatabs <2370315+jobatabs@users.noreply.github.com>
2024-12-02 19:59:13 +0200

    Tht 5 attempt

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -36,29 +36,50 @@ class All:
         return True
 
 class PlaysIn:
-    def __init__(self, team):
+    def __init__(self, matcher, team):
+        self._matcher = matcher
         self._team = team
 
     def test(self, player):
-        return player.team == self._team
+        return player.team == self._team and self._matcher.test(player)
 
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self.matcher = matcher
+    
+    def build(self):
+        return self.matcher
+    
+    def plays_in(self, team):
+        return QueryBuilder(PlaysIn(self.matcher, team))
+    
+    def has_at_least(self, value, attr):
+        return QueryBuilder(HasAtLeast(self.matcher, value, attr))
+    
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(HasFewerThan(self.matcher, value, attr))
+    
+    def one_of(self, matcher_1, matcher_2):
+        return QueryBuilder(matcher_1)
 
 class HasAtLeast:
-    def __init__(self, value, attr):
+    def __init__(self, matcher, value, attr):
+        self._matcher = matcher
         self._value = value
         self._attr = attr
 
     def test(self, player):
         player_value = getattr(player, self._attr)
 
-        return player_value >= self._value
+        return player_value >= self._value and self._matcher.test(player)
 
 class HasFewerThan:
-    def __init__(self, value, attr):
+    def __init__(self, matcher, value, attr):
+        self._matcher = matcher
         self._value = value
         self._attr = attr
 
     def test(self, player):
         player_value = getattr(player, self._attr)
 
-        return player_value < self._value
+        return player_value < self._value and self._matcher.test(player)
